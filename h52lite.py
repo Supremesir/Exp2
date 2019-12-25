87e27ee4921966c5768742c66401966768c3c7ff
Haofan Fang <haofanfang@gmail.com>
2019-12-25 11:35:31 +0800

    update h52lite.py

--- h52lite.py
+++ h52lite.py
@@ -1,2 +1,2 @@
-# 终端运行
-# tflite_convert --output_file=/Users/fang/PycharmProjects/Exp2/models/my_model.tflite --keras_model_file=/Users/fang/PycharmProjects/Exp2/models/my_model.h5
+# 终端(conda TensorFlow env)运行
+# tflite_convert --output_file=/Users/fang/PycharmProjects/Exp2/models/my_model.lite --keras_model_file=/Users/fang/PycharmProjects/Exp2/models/my_model.h5
